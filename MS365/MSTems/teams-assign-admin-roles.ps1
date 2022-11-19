199761019246e3fdcb4676bc131103d9f0a049aa
valatwork <105278308+valatwork@users.noreply.github.com>
2022-11-19 16:21:25 +0100

    Delete MS365/MSTems directory
    Signed-off-by: valatwork <105278308+valatwork@users.noreply.github.com>

--- MS365/MSTems/teams-assign-admin-roles.ps1
+++
@@ -1,5 +0,0 @@
-$userName="LynneR@contoso.com"
-$roleName="Teams Service Administrator"
-$role = Get-AzureADDirectoryRole | Where {$_.displayName -eq $roleName}
-
-Add-AzureADDirectoryRoleMember -ObjectId $role.ObjectId -RefObjectId (Get-AzureADUser | Where {$_.UserPrincipalName -eq $userName}).ObjectID
